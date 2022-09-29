164331526e848573fb32869961fdecd504962209
Mr. Kiedes <akiedes@vtsd.com>
2022-09-29 08:10:40 -0400

    final commit

---
+++ Lab03.java
@@ -0,0 +1,13 @@
+public class Lab03 {
+    public static void main(String args[]){
+        int startingValue = 10000;
+        int hours = startingValue / 3600;
+        int seconds = startingValue % 3600;
+
+        // Please do NOT do this
+        System.out.println("Starting seconds: " + startingValue);
+        System.out.println("Hours:      " + hours);
+
+    }
+
+}
