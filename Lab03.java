a43fb47c96ab0077419268e2d2bb8eef71eb517e
Antonio Martin <antonio.martin@vtsd.com>
2022-10-04 10:12:55 -0400

    Lab03

--- Lab03.java
+++ Lab03.java
@@ -8,7 +8,13 @@ public class Lab03 {
         System.out.println("startingValue: " % 3600);
         System.out.println("2800: " / 60);
         System.out.println("2800: " % 60);
-       
+
+        int startingValue = 10000;
+        int hours = startingValue / 3600;
+        int seconds = startingValue % 3600;
+        int minutes = seconds / 60;
+        int los = seconds % 60;
+
         // Please do NOT do this
         System.out.println("Starting seconds: " + startingValue);
         System.out.println("Hours:      " + hours);
